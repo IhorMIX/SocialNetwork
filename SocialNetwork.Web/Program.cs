d02c0aee4524da5e6ed4865eea182f868f52683b
jatcool <limpopo923@gmail.com>
2023-05-12 00:29:04 +0300

    Added Started project and created git.ignore file

---
+++ SocialNetwork.Web/Program.cs
@@ -0,0 +1,13 @@
+namespace SocialNetwork.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
