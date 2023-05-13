63a97de540ea50dfb4beca57427a8b3062547baf
Dzaikiro <40883582+Dzaikiro@users.noreply.github.com>
2023-05-14 01:04:08 +0300

    добавил конфигурацию логгера и подключил библиотеки

--- SocialNetwork.Web/Controllers/UserController.cs
+++ SocialNetwork.Web/Controllers/UserController.cs
@@ -11,13 +11,16 @@ namespace SocialNetwork.Web.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
+    private readonly ILogger<UserController> _logger;
+
     private readonly IUserService _userService;
     private readonly TokenHelper _tokenHelper;
 
-    public UserController(IUserService userService, TokenHelper tokenHelper)
+    public UserController(IUserService userService, TokenHelper tokenHelper, ILogger<UserController> logger)
     {
         _userService = userService;
         _tokenHelper = tokenHelper;
+        _logger = logger;
     }
 
     [AllowAnonymous]
@@ -25,6 +28,7 @@ public class UserController : ControllerBase
     public async Task<IActionResult> CreateUser([FromBody] UserCreateViewModel user,
         CancellationToken cancellationToken)
     {
+        _logger.LogInformation("start to create user");
         await _userService.CreateUserAsync(UserVieModelMapper.ConvertToBlModel(user), cancellationToken);
 
         return Ok();
@@ -40,5 +44,6 @@ public class UserController : ControllerBase
 
         return Ok(token);
     }
+   
     
 }
