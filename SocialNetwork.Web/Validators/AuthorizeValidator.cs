78ebd3cfcae28d1f8c86be5ce06cd777ee943611
JavaEE <dima.shevchenko10@gmail.com>
2023-05-14 02:20:48 +0300

    Added a validator for UserAuthorizeModel

---
+++ SocialNetwork.Web/Validators/AuthorizeValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SocialNetwork.Web.Models;
+
+namespace SocialNetwork.Web.Validators;
+
+public class AuthorizeValidator: AbstractValidator<UserAuthorizeModel>
+{
+    public AuthorizeValidator()
+    {
+        RuleFor(x => x.Login).NotEmpty().NotNull()
+            .Length(6,30)
+            .Matches("^[a-zA-Z0-9_-]*$")
+            .WithMessage("Not a valid login format. Only Latin letters, digits, underscore and hyphen are allowed.");
+        
+        RuleFor(x => x.Password).NotEmpty().NotNull()
+            .Length(8, 50)
+            .Matches("^[a-zA-Z0-9]*$")
+            .WithMessage("Not a valid password format. Only Latin letters and digits are allowed.");
+
+    }
+}
