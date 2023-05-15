1b3708205edb330a7e3e1b81f8e52a75630a724d
jatcool <limpopo923@gmail.com>
2023-05-15 13:38:03 +0300

    Added autoMapperConfig

---
+++ SocialNetwork.Web/MapperConfiguration/MapperModelsConfig.cs
@@ -0,0 +1,24 @@
+﻿using SocialNetwork.BL.Models;
+using SocialNetwork.DAL.Entity;
+using SocialNetwork.Web.Models;
+using Profile = AutoMapper.Profile;
+
+namespace SocialNetwork.Web.MapperConfiguration
+{
+    public class MapperModelsConfig : Profile
+    {
+        public MapperModelsConfig()
+        {
+            CreateMap<UserCreateViewModel, UserModel>();
+            CreateMap<ProfileCreateViewModel, ProfileModel>();
+            
+            CreateMap<UserModel, User>()
+                .ForMember(dest => dest.ProfileId, opt => opt.Ignore())
+                .ForMember(dest => dest.AuthorizationInfoId, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<AuthorizationInfoModel, AuthorizationInfo>().ReverseMap();
+            CreateMap<ProfileModel, SocialNetwork.DAL.Entity.Profile>().ReverseMap();
+        }
+    }
+}
