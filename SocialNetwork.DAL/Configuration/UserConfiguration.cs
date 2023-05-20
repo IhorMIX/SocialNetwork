5cf43cc1eb73b7298beb2ecd38dda6d07537bd31
Dmitry <dima.shevchenko10@gmail.com>
2023-05-21 01:53:32 +0300

    added fields to make relation friend

--- SocialNetwork.DAL/Configuration/UserConfiguration.cs
+++ SocialNetwork.DAL/Configuration/UserConfiguration.cs
@@ -17,5 +17,11 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.HasOne(i => i.AuthorizationInfo)
             .WithOne(i => i.User)
             .HasForeignKey<AuthorizationInfo>(i => i.UserId);
+        
+        builder.HasMany(u => u.Friends)
+            .WithOne(f => f.User)
+            .HasForeignKey(f => f.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
     }
 }
