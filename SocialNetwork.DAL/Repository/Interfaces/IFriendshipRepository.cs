8678a9da0c629586cc3b27014e3f304b22c41460
Dmitry <dima.shevchenko10@gmail.com>
2023-05-21 01:50:33 +0300

    added friendship repository

---
+++ SocialNetwork.DAL/Repository/Interfaces/IFriendshipRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetwork.DAL.Entity;
+
+namespace SocialNetwork.DAL.Repository.Interfaces;
+
+public interface IFriendshipRepository : IBasicRepository<Friendship>
+{
+    Task<bool> DeleteFriendsAsync(Friendship friendship, CancellationToken cancellationToken = default);
+
+    Task CreateFriendshipAsync(Friendship friendship, CancellationToken cancellationToken = default);
+    
+    Task<Friendship?> GetByFriendIdsAsync(Friendship friendship, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<User>> GetAllFriendsAsync(int id, CancellationToken cancellationToken = default);
+    
+}
