7ac16b13b6b07d389f6f657b76ddee495e020418
Dmitry <dima.shevchenko10@gmail.com>
2023-05-21 01:51:45 +0300

    added entity, model and configuration

---
+++ SocialNetwork.BL/Models/FriendshipModel.cs
@@ -0,0 +1,11 @@
+﻿using SocialNetwork.DAL.Entity;
+
+namespace SocialNetwork.BL.Models;
+
+public class FriendshipModel
+{
+    public int UserId { get; set; }
+    public int FriendId { get; set; }
+    public UserModel? UserModel { get; set; }
+    public UserModel? FriendUserModel { get; set; }
+}
