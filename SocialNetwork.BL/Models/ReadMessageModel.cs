d75cdd24d10fec759ea1f59ffb18f80476148256
Dmitry <dima.shevchenko10@gmail.com>
2023-10-03 04:55:38 +0300

    models

---
+++ SocialNetwork.BL/Models/ReadMessageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialNetwork.BL.Models;
+
+public class ReadMessageModel
+{
+    public DateTime ReadAt { get; set; }
+    
+    public int ChatMemberId { get; set; }
+
+    public int MessageId { get; set; }
+    
+    public MessageModel Message { get; set; }
+
+    public ChatMemberModel ChatMember { get; set; }
+}
