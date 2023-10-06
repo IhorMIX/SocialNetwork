28427498e22e98313447c59010720d61a2db5d12
Dmitry <dima.shevchenko10@gmail.com>
2023-10-06 16:11:34 +0300

    message migration

---
+++ SocialNetwork.DAL/Migrations/20231005075741_Message_Migration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Message_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReadMessages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReadMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatMemberId = table.Column<int>(type: "int", nullable: false),
+                    MessageId = table.Column<int>(type: "int", nullable: false),
+                    ReadAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReadMessages_ChatMembers_ChatMemberId",
+                        column: x => x.ChatMemberId,
+                        principalTable: "ChatMembers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReadMessages_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReadMessages_ChatMemberId",
+                table: "ReadMessages",
+                column: "ChatMemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReadMessages_MessageId",
+                table: "ReadMessages",
+                column: "MessageId");
+        }
+    }
+}
