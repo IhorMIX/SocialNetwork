1766fdbc5fa36ee58cff2f3ad1126794bb0de2d4
Dmitry <dima.shevchenko10@gmail.com>
2023-10-18 19:31:24 +0300

    check file collection on null or empty

--- SocialNetwork.BL/Services/MessageService.cs
+++ SocialNetwork.BL/Services/MessageService.cs
@@ -42,7 +42,7 @@ public class MessageService : IMessageService
         {
             ChatAccess.SendMessages
         };
-        if (messageModel.FileModels.Any())
+        if (messageModel.FileModels?.Any() == true)
         {
             access.Add(ChatAccess.SendFiles);
         }
@@ -115,7 +115,7 @@ public class MessageService : IMessageService
         {
             ChatAccess.SendMessages
         };
-        if (messageModel.FileModels.Any())
+        if (messageModel.FileModels?.Any() == true)
         {
             access.Add(ChatAccess.SendFiles);
         }
@@ -160,7 +160,7 @@ public class MessageService : IMessageService
         {
             ChatAccess.SendMessages
         };
-        if (messageModel.FileModels.Any())
+        if (messageModel.FileModels?.Any() == true)
         {
             access.Add(ChatAccess.SendFiles);
         }
