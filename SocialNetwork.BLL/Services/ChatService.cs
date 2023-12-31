614020f9be612eec5390e697ecc146556dc07181
Dmitry <dima.shevchenko10@gmail.com>
2023-12-31 19:55:44 +0200

    inject INotificationRepository

--- SocialNetwork.BLL/Services/ChatService.cs
+++ SocialNetwork.BLL/Services/ChatService.cs
@@ -26,6 +26,7 @@ public class ChatService : IChatService
     private readonly IChatMemberRepository _chatMemberRepository;
     private readonly IMapper _mapper;
     private readonly RoleOption _roleOptions;
+    private readonly INotificationRepository _notificationRepository;
 
     public ChatService(
         IChatRepository chatRepository,
