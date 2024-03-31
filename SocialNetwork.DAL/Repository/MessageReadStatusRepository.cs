22b8f297cdd31991792a9a0e496bdba115f8e5ad
Dmitry <dima.shevchenko10@gmail.com>
2024-03-31 17:50:58 +0300

    Delete comment

--- SocialNetwork.DAL/Repository/MessageReadStatusRepository.cs
+++ SocialNetwork.DAL/Repository/MessageReadStatusRepository.cs
@@ -36,29 +36,6 @@ public class MessageReadStatusRepository : IMessageReadStatusRepository
         await _socialNetworkDbContext.SaveChangesAsync(cancellationToken);
     }
 
-    // public async Task UpdateStatus(IEnumerable<MessageReadStatus> messageReadStatuses, CancellationToken cancellationToken = default)
-    // {
-    //     
-    //     foreach (var messageReadStatus in messageReadStatuses)
-    //     {
-    //         // Load the existing entity from the database
-    //         var existingMessageReadStatus = await _socialNetworkDbContext.MessageReadStatuses
-    //             .FirstOrDefaultAsync(mrs => mrs.ChatMemberId == messageReadStatus.ChatMemberId && mrs.MessageId == messageReadStatus.MessageId, cancellationToken);
-    //
-    //         if (existingMessageReadStatus != null)
-    //         {
-    //             // Update only the IsRead property
-    //             existingMessageReadStatus.IsRead = messageReadStatus.IsRead;
-    //             // You can also update other properties if needed
-    //             // Mark the entity as modified
-    //             _socialNetworkDbContext.Entry(existingMessageReadStatus).State = EntityState.Modified;
-    //         }
-    //     }
-    //
-    //     await _socialNetworkDbContext.SaveChangesAsync(cancellationToken);
-    //
-    //     
-    // }
     public async Task<IEnumerable<MessageReadStatus>> UpdateStatus(IEnumerable<MessageReadStatus> messageReadStatuses, CancellationToken cancellationToken = default)
     {
         var updatedMessageReadStatuses = new List<MessageReadStatus>();
